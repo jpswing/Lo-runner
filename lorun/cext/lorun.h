0721da031a9a6712d917d26a64e554e743827fb5
jp <41479689+jpswing@users.noreply.github.com>
2020-02-24 20:13:13 +0800

    change MAX_OUTPUT

--- lorun/cext/lorun.h
+++ lorun/cext/lorun.h
@@ -22,7 +22,7 @@
 #include <Python.h>
 #include <sys/types.h>
 #define CALLS_MAX 400
-#define MAX_OUTPUT 100000
+#define MAX_OUTPUT (60 * 1024 * 1024)
 
 enum JUDGE_RESULT {
     AC=0,   //0 Accepted
